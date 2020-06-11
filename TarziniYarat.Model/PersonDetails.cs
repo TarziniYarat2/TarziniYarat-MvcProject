23e8875030a23c099d6ff0fae58e98615aa6f2e0
Tuğba ÖZMEN <65816851+tugbaozmen@users.noreply.github.com>
2020-06-11 13:32:56 +0300

    Persondetail

--- TarziniYarat.Model/PersonDetails.cs
+++ TarziniYarat.Model/PersonDetails.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,23 @@ namespace TarziniYarat.Model
 {
     public class PersonDetails:BaseEntity
     {
+        
         public int PersonID { get; set; }
+        [Required(ErrorMessage = "Bu alan boş geçilemez.")]
+        [DisplayName("Ülke")]
         public string Country { get; set; }
+
+        [DisplayName("Şehir")]
+        [Required(ErrorMessage = "Bu alan boş geçilemez.")]
         public string City { get; set; }
+
+        [DisplayName("Adres")]
+        [Required(ErrorMessage = "Bu alan boş geçilemez.")]
         public string Address { get; set; }
+
+        [DisplayName("Telefon Numarası")]
+        [Required(ErrorMessage = "Bu alan boş geçilemez.")]
+        [MinLength(11)]
         public string PhoneNumber { get; set; }
 
         //Navigation prop
